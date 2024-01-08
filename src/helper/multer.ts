3ecfc5de5e141643c4a8ebe2370ef547226f1aa1
Dendi <dendiaa@students.amikom.ac.id>
2024-01-09 00:39:32 +0700

    fix: typo in multer destionation

--- src/helper/multer.ts
+++ src/helper/multer.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import { v4 as uuid } from "uuid";
 
 export const storage = multer.diskStorage({
-  destination: "./tmp",
+  destination: "/tmp",
   filename: (req, file, cb) => {
     const uniqueFileName = `${uuid()}-${file.originalname}`;
     cb(null, uniqueFileName);
