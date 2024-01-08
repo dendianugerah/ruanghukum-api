a6177ca3c85e2916722ebb47bdad212914a802d4
Dendi <dendiaa@students.amikom.ac.id>
2024-01-09 00:47:59 +0700

    fix: deployment issue

--- src/config/gcs.ts
+++ src/config/gcs.ts
@@ -1,10 +1,13 @@
 import { Storage } from "@google-cloud/storage";
 
+const b64_priv_key = process.env.GOOGLE_PRIVATE_KEY;
+const buff = Buffer.from(b64_priv_key as string).toString("base64");
+
 export const googleStorage = new Storage({
   projectId: "hackfest-bebasss",
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: `${buff}`,
   },
 });
 
