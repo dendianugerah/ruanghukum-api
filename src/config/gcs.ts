4229985a065f94a3dcf1d4e841030483a8e4d5b5
Dendi <dendiaa@students.amikom.ac.id>
2024-01-09 01:02:42 +0700

    fix: gcs process env

--- src/config/gcs.ts
+++ src/config/gcs.ts
@@ -1,13 +1,18 @@
 import { Storage } from "@google-cloud/storage";
 
-const b64_priv_key = process.env.GOOGLE_PRIVATE_KEY;
-const buff = Buffer.from(b64_priv_key as string).toString("base64");
+if (!process.env.GOOGLE_PRIVATE_KEY) {
+  throw new Error(
+    "GOOGLE_PRIVATE_KEY is not defined in the environment variables."
+  );
+}
 
 export const googleStorage = new Storage({
   projectId: "hackfest-bebasss",
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: `${buff}`,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join(
+      "\n"
+    ),
   },
 });
 
